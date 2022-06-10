ca6a3df8a489db9c63cfc26db6d70e1aa9800b22
Jason9789 <lifebook0809@gmail.com>
2022-06-10 16:52:06 +0900

    add: Sign-in and Login

--- blog-fe/src/components/auth/AuthForm.js
+++ blog-fe/src/components/auth/AuthForm.js
@@ -55,12 +55,22 @@ const Footer = styled.div`
   }
 `;
 
+/**
+ * 에러를 보여줌
+ */
+const ErrorMessage = styled.div`
+  color: red;
+  text-align: center;
+  font-size: 0.875rem;
+  margin-top: 1rem;
+`;
+
 const textMap = {
   login: '로그인',
   register: '회원가입',
 };
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = textMap[type];
 
   return (
@@ -92,6 +102,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
             value={form.passwordConfirm}
           />
         )}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <ButtonWithMarginTop cyan fullWidth>
           {text}
         </ButtonWithMarginTop>
