729244d295db90c67e3b058129c48055a0d01672
Jason9789 <lifebook0809@gmail.com>
2022-06-11 20:54:15 +0900

    edit: Procfile

--- blog-be/src/main.js
+++ blog-be/src/main.js
@@ -6,7 +6,6 @@ import mongoose from 'mongoose';
 import serve from 'koa-static';
 import path from 'path';
 import send from 'koa-send';
-import cors from 'cors';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -29,7 +28,7 @@ const router = new Router();
 // 라우터 설정
 router.use('/api', api.routes()); // api 라우트 적용
 
-app.use(cors());
+// app.use(cors());
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
@@ -47,7 +46,7 @@ app.use(async (ctx) => {
   }
 });
 
-const port = process.env.PORT || 4000;
+const port = PORT || 4000;
 app.listen(port, () => {
   console.log('Listening to port %d', port);
 });
